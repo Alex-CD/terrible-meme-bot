a1bb5e93659bab27aefdd51a2f49af698207f1e4
Alexander Clarke <alexander_m_clarke@live.co.uk>
2020-04-27 14:08:16 +0100

    Add hyt command

--- src/commands/play.js
+++ src/commands/play.js
@@ -6,7 +6,7 @@ var bot = require('../botUtils');
 
 class Play {
     constructor(players){
-        this.aliases = ["play"];
+        this.aliases = ["play", "hyt"];
         this.players = players;
     }
 
@@ -20,6 +20,11 @@ class Play {
         if(command == ""){
             this.players.resume(message);
             return;
+        }
+
+        // if message is 'hidden youtube'
+        if(message.content.slice(1).split(' ')[0] === "hyt"){
+            await message.delete();
         }
 
         this.players.play(message, command);
