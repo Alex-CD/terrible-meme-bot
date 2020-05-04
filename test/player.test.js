9ee58306524a5e6de36db6272b0bf6f841e11d93
Alexander Clarke <alexander_m_clarke@live.co.uk>
2020-05-04 15:46:31 +0100

    Adjust message stubs, add tests for player

--- test/player.test.js
+++ test/player.test.js
@@ -1,4 +1,5 @@
 var assert = require('chai').assert;
+var util = require('util');
 
 const Player = require('../src/player');
 
@@ -29,10 +30,29 @@ describe("player", function(){
     });
 
     describe("#waitToLeave", function(){
-        it("should disconnect the bot if the bot waits for long enough", function(){
-            player = new Player(0.5);
-            player.waitToDisconnect();
-        })
+        it("should disconnect the bot if the bot waits for long enough", async function(){
+            player = new Player();
+            player.idleDisconnectDelayMS = 30;
+
+            var message = new MessageStub();
+            
+            await player.waitToDisconnect(message);
+            await util.promisify(setTimeout)(1500);
+            
+            assert(message.voiceChannel == null, "The bot's channel should have been set to null");
+        });
+
+        it("should not disconnect the bot if lastPlay is changed", async function(){
+            player = new Player();
+            player.idleDisconnectDelayMS = 100;
+
+            var message = new MessageStub();
+            await player.waitToDisconnect(message);
+            player.lastFinishTime = Date.now();
+            await util.promisify(setTimeout)(100);
+            
+            assert(message.state.voiceChannel == "000", "The bot's channel should have been left unchanged");
+        });
     });
 
 
